63affdd14dc6f67faf1a9ff30dc8c8cb82e31894
aliserhan315 <aliserhan315@gmail.com>
2024-12-11 20:40:09 +0200

    trying to host

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'Tail-Pix', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
