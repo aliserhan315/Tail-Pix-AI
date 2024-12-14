8d2911ce52e2cb7b18c3a4da3d0916d232a82ea9
aliserhan315 <aliserhan315@gmail.com>
2024-12-14 19:22:36 +0200

    middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,8 @@ export default authMiddleware({
 });
  
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    '/((?!.*\\..*|_next).*)','/'
+   , '/(api|trpc)(.*)'
+  ]
+}
